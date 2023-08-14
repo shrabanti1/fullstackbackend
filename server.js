e182375ec14933169e2e8c755acca827111f6cf6
shrabanti1 <jharnamazumdar78623@gmail.com>
2023-08-14 23:58:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use("https://ecommerce-backend-s1yc.onrender.com/api/v1/auth", authRoutes);
-app.use("https://ecommerce-backend-s1yc.onrender.com/api/v1/category", categoryRoutes);
-app.use("https://ecommerce-backend-s1yc.onrender.com/api/v1/product", productRoutes);
+app.use("api/v1/auth", authRoutes);
+app.use("api/v1/category", categoryRoutes);
+app.use("api/v1/product", productRoutes);
 
 //rest api
 app.get("/", (req, res) => {
